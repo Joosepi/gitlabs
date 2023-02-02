4665f973e672ccfebd5035e1402211b0e12f4358
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-02-02 14:08:13 +0200

    HALFWAYDONE

---
+++ gitlab.js
@@ -0,0 +1,12 @@
+// create an image element
+var image = document.createElement("img");
+
+// set image source
+img.src = "/_nuxt/image/f94c74.png";
+
+// set image width and height
+image.width = 500;
+image.height = 300;
+
+// append image to the image container element
+document.getElementById("image-container").appendChild(image);
